3e31d07778bf7bb88283199bba5bff6c7746e669
Ksenia Razheva <pokatskayakseniya@gmail.com>
2024-02-28 09:58:00 +0300

    add l.23 for in for and label

--- src/js/index.js
+++ src/js/index.js
@@ -290,3 +290,89 @@
 // 	console.log('Done!');
 // }
 //Done
+
+
+//урок 23 цикл в цикле и метки
+
+
+// for (let i = 0; i < 3; i++) {
+// 	console.log(i);
+// 	for (let j = 0; j < 3; j++) {
+// 		console.log(j);
+// 	}
+// }
+// //0 - первый цикл
+// //0 - второй цикл
+// //1 - второй цикл
+// //2 - второй цикл доходит до 2 меньше 3
+// //1 - первый цикл 0+1=1
+// //0 - начинается второй цикл
+// //1 - второй цикл
+// //2 - второй цикл доходит до 2 меньше 3
+// //2 - начинается первый цикл 1+1=2 (первый цикл дает ответ в последний раз, i = 2 это меньше 3)
+// //0 - начинается второй цикл с 0
+// //1 - второй цикл
+// //2 - второй цикл доходит до j= 2 это меньше 3, а первый цикл не начинается потому что он уже вернул ранее 2 меньше 3
+
+
+// let result = '';
+// const length = 7;
+
+// for (let i = 1; i < length; i++) {
+
+// 	for (let j = 0; j < i; j++) {
+// 		result += '*';
+// 	}
+
+// 	result += '\n';
+// }
+
+// console.log(result);
+// //*
+// //**
+// //***
+// //****
+// //*****
+// //******
+
+
+// first: for (let i = 0; i < 3; i++) {
+// 	console.log(`First level: ${i}`);
+// 	for (let j = 0; j < 3; j++) {
+// 		console.log(`Second level: ${j}`);
+// 		for (let k = 0; k < 3; k++) {
+// 			if (k === 2) continue first;
+// 			console.log(`Third level: ${k}`);
+// 		}
+// 	}
+// }
+// //First level: 0
+// //Second level: 0
+// //Third level: 0
+// //Third level: 1
+// //First level: 1
+// //Second level: 0
+// //Third level: 0
+// //Third level: 1
+// //First level: 2
+// //Second level: 0
+// //Third level: 0
+// //Third level: 1
+// //третий уровень доходит только до 1 меньше 2
+
+
+// first: for (let i = 0; i < 3; i++) {
+// 	console.log(`First level: ${i}`);
+// 	for (let j = 0; j < 3; j++) {
+// 		console.log(`Second level: ${j}`);
+// 		for (let k = 0; k < 3; k++) {
+// 			if (k === 2) break first;
+// 			console.log(`Third level: ${k}`);
+// 		}
+// 	}
+// }
+// //First level: 0
+// //Second level: 0
+// //Third level: 0
+// //Third level: 1
+
