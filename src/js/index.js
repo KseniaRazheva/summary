c57c17261dfcb30a67a43bfa0567ae392f598a0d
Ksenia Razheva <pokatskayakseniya@gmail.com>
2024-02-28 17:11:42 +0300

    add ex.4 cycles and conditions

--- src/js/index.js
+++ src/js/index.js
@@ -463,3 +463,59 @@
 // return arrayOfNumbers; 
 // //parding error: 'return'outside of funstion eslint - добавила в .eslintrc.js в parserOptions - ecmaFeatures - globalReturn true
 // //[ 5, 6, 7, 8, 9, 10 ]
+
+
+//упражнение по написанию кода 4 * продвинутые задания на использование циклов и условий
+//Задачи:
+
+
+// //1. Заполните новый массив (result) числами из старого (arr). Количество элементов в массиве можно получить как arr.length, а к элементам обращаемся все так же: arr[0], arr[1] и тд. Должен получиться точно такой же массив
+// // Место для первой задачи
+// // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+// const arr = [3, 5, 8, 16, 20, 23, 50];
+// const result = [];
+// for (let i = 0; i < arr.length; i++) {
+// 	result[i] = arr[i];
+// }
+// console.log(result);
+// return result;
+// //[
+// //    3,  5,  8, 16,
+// //   20, 23, 50
+// // ]
+
+
+// // 2.Измените данный массив так, чтобы все числа были увеличены в 2 раза, а если попадается строка строка - то к ней было добавлено " - done".
+// // Для определения типа данных используйте typeof();
+// // Должно получиться: [ 10, 20, 'Shopping - done', 40, 'Homework - done' ]
+// // Место для второй задачи
+// // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+// const data = [5, 10, 'Shopping', 20, 'Homework'];
+// for (let i = 0; i < data.length; i++) {
+// 	if (typeof(data[i]) === 'number') {
+// 		data[i] = data[i] * 2;
+// 	} else if (typeof(data[i]) === 'string') {
+// 		data[i] = `${data[i]} - done`;
+// 	}
+// }
+// console.log(data); 
+// return data;
+// //[ 10, 20, 'Shopping - done', 40, 'Homework - done' ]
+
+
+// //3. Разверните массив data наоборот при помощи цикла и запишите данные в массив result. 
+// //Должно получиться: [ 'Homework', 20, 'Shopping', 10, 5 ]
+// // Место для третьей задачи
+// // Значения массива менять нельзя, тут он проверяется автоматически именно на эти значения
+// const data = [5, 10, 'Shopping', 20, 'Homework'];
+// const result = [];
+
+// for (let i = 1; i <= data.length; i++) {
+// 	result[i - 1] = data[data.length - i];
+// }
+    
+// console.log(result);
+// return result;
+// //[ 'Homework', 20, 'Shopping', 10, 5 ]
+
+
