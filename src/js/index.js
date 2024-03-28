83bc50d5b69b268b8af1dc3ef87118800aea827a
Ksenia Razheva <pokatskayakseniya@gmail.com>
2024-03-28 18:52:42 +0300

    add ex11-12 arrays

--- src/js/index.js
+++ src/js/index.js
@@ -1236,3 +1236,132 @@
 // //Язык php изучен на 10%
 
 
+
+//упражнение по написанию кода 11: задачи на работу с массивами
+//Задачи:
+// //1) Напишите функцию showFamily, которая будет принимать в себя массив строк и возвращать сообщение в нужном формате.
+// //showFamily(family)  => 'Семья состоит из: Peter Ann Alex Linda'
+// //Имена подставляются автоматически из массива. Если массив пустой, то выводится сообщение 'Семья пуста'
+// const family = ['Peter', 'Ann', 'Alex', 'Linda'];
+// function showFamily(arr) {
+//     let str = '';
+//     arr.length === 0 ? str = 'Семья пуста' : str = 'Семья состоит из: ';
+//     arr.forEach(member => {
+//         str += `${member} `
+//     });
+//     return str;
+// }
+// console.log(showFamily(family));//Семья состоит из: Peter Ann Alex Linda 
+
+
+// //2) напишите функцию standardizeStrings, которая будет принимать в себя массив строк и будет выводить в консоль эти строки в нижнем регистре.
+// //Пример:
+// //standardizeStrings(favoriteCities)  выведет в консоль
+// //lisbon
+// //rome
+// //milan
+// //dublin
+// const favoriteCities = ['liSBon', 'ROME', 'miLan', 'Dublin'];
+// function standardizeStrings(arr) {
+//     arr.forEach(city => {
+//         console.log(city.toLowerCase())
+//     })
+// }
+// console.log(standardizeStrings(favoriteCities));
+// //lisbon
+// //rome
+// //milan
+// //dublin
+// //undefined
+
+
+
+// упражнение по написанию кода 12 задачи на работу с массивами часть 2
+// 3) Задача с собеседований. Напишите функцию reverse, которая принимает в себя строку и возвращает эту строку в обратном порядке.
+// Пример:
+// const someString = 'This is some strange string';
+// reverse(someString) => 'gnirts egnarts emos si sihT'
+// Функцию можно применить к любой строке. Если в функцию приходит не строка - вернуть сообщение "Ошибка!"
+// Метод reverse() на месте обращает порядок следования элементов массива. Первый элемент массива становится последним, а последний — первым.
+
+// //Оптимальный вариант решения:
+// const someString = 'This is some strange string';
+// function reverse(str) {
+//     if (typeof(str) !== 'string') {
+//         return "Ошибка!";
+//     }
+//     return str.split('').reverse().join('');
+// }
+// console.log(reverse(someString));//gnirts egnarts emos si sihT
+
+// //решение при помощи цикла:
+// const someString = 'This is some strange string';
+// function reverse(str) {
+//     let newStr = '';
+//     for (let i = str.length - 1; i >= 0; i--) {
+//         newStr += str[i];
+//     }
+//     return newStr
+// }
+// console.log(reverse(someString));//gnirts egnarts emos si sihT
+
+
+
+// 4) Представьте такую реальную ситуацию. У вас есть банкомат, который выдает деньги из двух разных банков в разных валютах. Один банк основной с базовыми валютами, второй дополнительный с прочими валютами:
+// const baseCurrencies = ['USD', 'EUR'];
+// const additionalCurrencies = ['UAH', 'RUB', 'CNY'];
+// Вам нужно создать главную функцию банкомата availableCurr, которая принимает два аргумента: первый - это массив со всеми доступными валютами из двух банков сразу (сейчас представим, что они не могут повторяться), второй - необязательный аргумент, который указывает ту валюту, которая сейчас закончилась в банкомате. Если массив в первом аргументе пустой - то функция возвращает строку 'Нет доступных валют'. Функция возвращает строку в нужном виде.
+// Пример:
+// availableCurr(['UAH', 'RUB', 'CNY'], 'CNY')
+// Вернет строку:
+// Доступные валюты:
+// UAH
+// RUB
+// Заметьте:
+// - CNY (юань) исчез из списка валют, значит такая валюта закончилась
+// - После валюты: стоит перенос строки \n, и после каждой валюты тоже. Это важно для тестов
+// - Данные для первого аргумента должны приходить сразу из двух банков, причем сначала baseCurrencies, потом additionalCurrencies по порядку
+
+// //оптимальное решение:
+// const baseCurrencies = ['USD', 'EUR'];
+// const additionalCurrencies = ['UAH', 'RUB', 'CNY'];
+// function availableCurr(arr, missingCurr) {
+//     let str = '';
+//     arr.length === 0 ? str = 'Нет доступных валют' : str = 'Доступные валюты:\n';
+//     arr.forEach(function(curr, i) {
+//         if (curr != missingCurr) {
+//             str += `${curr}\n`;
+//         }
+//     });
+//     return str;
+// }
+// console.log(availableCurr([...baseCurrencies, ...additionalCurrencies], 'CNY'))
+// //Доступные валюты:
+// //USD
+// //EUR
+// //UAH
+// //RUB
+
+// //с помощью цикла фор
+// const baseCurrencies = ['USD', 'EUR'];
+// const additionalCurrencies = ['UAH', 'RUB', 'CNY'];
+// function availableCurr(arr, missingCurr) {
+//     let str = '';
+//     arr.length === 0 ? str = 'Нет доступных валют' : str = 'Доступные валюты:\n';
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] === missingCurr) {
+//             continue;
+//         }
+//         str += `${arr[i]}\n`;
+//     }
+//     return str;
+// }
+// console.log(availableCurr([...baseCurrencies, ...additionalCurrencies], 'CNY'));
+// //Доступные валюты:
+// //USD
+// //EUR
+// //UAH
+// //RUB
+
+
+
