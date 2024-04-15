725319e6648ea13e72a01e097212a914ccdf3e18
Ksenia Razheva <pokatskayakseniya@gmail.com>
2024-04-15 17:57:13 +0300

    add ex.17,18,19

--- src/js/index.js
+++ src/js/index.js
@@ -2207,3 +2207,106 @@ function loadScript(src) {
 // loadScript("js/script.js"); почему-то здесь не подключает js в котором куча всего, только новые в которых 1 запись:
 loadScript("js/some.js");
 loadScript("js/test.js");
+
+
+
+//упражнение по написанию кода 17*** сложная задача на вычисление количества страниц
+//Задание:
+//В каждой книге есть n страниц с номерами страниц от 1 до n. Написать функцию amountOfPages, аргумент которой summary составляется путем сложения количества цифр всех номеров страниц. Эта функция возвращает число - количество страниц n в книге. Чтобы было понятно что такое количество цифр, давайте рассмотрим примеры.
+//Пример:
+//Если на входе функции summary = 25, то на результат должен быть 17. Всего в числах от 1 до 17 содержится 25 цифр: 1234567891011121314151617.
+//Функция на вход как раз принимает это общее количество цифр, а возвращает конечное число, то есть последнюю страницу книги.
+//amountOfPages(5) => 5
+//amountOfPages(25) => 17
+//amountOfPages(1095) => 401   
+//amountOfPages(185) => 97
+
+function amountOfPages(summary) {
+    let result = '';
+    let n = 0;
+
+    for (let i = 1; i <= summary; i++) {
+        result += i;
+        if (result.length === summary) {
+            n = i;
+            break;
+        }
+    }
+    return n;
+}
+
+
+
+//Упражнение по написанию кода 18** Сложная задача на работу со строками
+//Задание:
+//Панграмма — это предложение, в котором каждая буква алфавита встречается хотя бы по одному разу по возможности без повторений. Например, предложение «The quick brown fox jumps over the lazy dog» является панграммой, поскольку в нем хотя бы один раз используются буквы от A до Z (регистр значения не имеет).
+//Напишите функцию isPangram, которая принимает в себя строку и возвращает логическое значение. Если строка является панграммой - вернется true, если нет - false.
+//Пример:
+//isPangram(«The quick brown fox jumps over the lazy dog») => true
+//isPangram(«Hello world») => false
+
+//1 вариант, когда строка переводится в нижний регистр до всех операций только 1 раз - это должно экономить ресурсы компьютера
+function isPangram(string) {
+    string = string.toLowerCase();
+    return "abcdefghijklmnopqrstuvwxyz".split("").every(function(x){
+        return string.indexOf(x) !== -1;
+    });
+}
+
+//2 вариант - с другим методом и строка каждый раз преобразовывается в коллбэке
+function isPangram(string) {
+    return 'abcdefghijklmnopqrstuvwxyz'
+        .split('')
+        .every((x) => string.toLowerCase().includes(x));
+}
+
+//3 вариант - при помощи цикла
+function isPangram(str) {
+    letters: for (var c = 0; c < 26; c++) {
+        for (let i = 0; i < str.length; i++) {
+            let s = str.charCodeAt(i)
+            if (s < 65 || s > 90 && s < 97 || s > 122) continue
+            if (s === 65 + c || s === 97 + c) continue letters
+        }
+
+        return false
+    }
+
+    return true
+}
+
+//4 вариант - при помощи set
+function isPangram(string) {
+    return new Set(string.toLocaleLowerCase().replace(/[^a-z]/gi, '').split('')).size === 26;
+}
+
+//5 вариант - с использованием регулярных выражений
+function isPangram(string) {
+    return (string.match(/([a-z])(?!.*\1)/ig) || []).length === 26;
+}
+
+
+
+//упражнение по написанию кода 19** сложная задача на работу с рекурсией
+//Задание:
+//Создайте функцию deepCount, которая будет считать количество всех элементов в массиве, включая и вложенные массивы. Учтите, что сам вложенный массив тоже входит в счет. Чтобы понять задачу детальнее, давайте рассмотрим примеры:
+//deepCount([1, 5, 3]) => 3
+//deepCount(["1", 5, "3", ["10"]]) => 5 (Заметьте, что последний элемент был посчитан сам + его внутренность)
+//deepCount([1, 2, [3, 4, [5]]]) => 7
+//deepCount([]) => 0
+//deepCount([[[[[[[[[]]]]]]]]]) => 8
+
+//1 вариант с циклом
+function deepCount(a) {
+    let count = a.length;
+    for (let i = 0; i < a.length; i++) if (Array.isArray(a[i])) count += deepCount(a[i]);
+    return count;
+}
+
+//2 вариант в методом reduce
+function deepCount(a) {
+    return a.reduce((s,e)=>s+(Array.isArray(e) ? deepCount(e) : 0),a.length);
+}
+
+
+
